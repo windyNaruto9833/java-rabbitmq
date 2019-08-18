7292ef2c817c8ce6e33feb41164949502c1e5a65
windyNaruto9833 <1060530388@qq.com>
2019-08-18 11:56:59 +0800

    update rabbitmq topic

--- src/main/java/com/windy/mq/topicMq/ReceiverTopic.java
+++ src/main/java/com/windy/mq/topicMq/ReceiverTopic.java
@@ -1,4 +1,4 @@
-package com.windy.rabbitmq.topic;
+package com.windy.mq.topicMq;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
@@ -11,14 +11,12 @@ import org.springframework.stereotype.Component;
 public class ReceiverTopic {
     @RabbitListener(queues="topic.message111")    //监听器监听指定的Queue
     public void process1(String str) {
-        System.out.println("message:"+str);
+        System.out.println("message111收到消息:"+str);
     }
 
     @RabbitListener(queues="topic.messages111")    //监听器监听指定的Queue
     public void process2(String str) {
-        System.out.println("messages:"+str);
+        System.out.println("messages111收到消息:"+str);
     }
-
-
 
 }
